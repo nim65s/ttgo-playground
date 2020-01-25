617146835cf89d781e7caa0994afcd3dc078a69f
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-01-25 14:47:13 +0100

    blink

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -11,11 +11,18 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 2
 
 
 void app_main()
 {
-    printf("Hello Baroustan!\n");
+    printf("Hello Baroustan blink!\n");
+
+    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(BLINK_GPIO, 0);
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -31,6 +38,13 @@ void app_main()
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
     for (int i = 10; i >= 0; i--) {
+        for (int j = 0; j <= i ; j++) {
+            gpio_set_level(BLINK_GPIO, 1);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            gpio_set_level(BLINK_GPIO, 0);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
